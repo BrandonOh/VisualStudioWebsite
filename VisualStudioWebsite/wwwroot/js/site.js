e94dbd4ab2490ef8b218e0af9a25fd6c04ebe39a
Brandon Oh <brandonoh2938@gmail.com>
2022-03-13 22:54:45 -0400

    Add project files.

---
+++ VisualStudioWebsite/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function MoveIcon() {
+    let id = null;
+    const elem = document.getElementById("myAnimation")
+    let pos = 4;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 550) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
